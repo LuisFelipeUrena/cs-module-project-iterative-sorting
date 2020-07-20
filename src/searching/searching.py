825914ae288ee38ae359f6543df564fa076ac7e0
LuisFelipeUrena <60860402+LuisFelipeUrena@users.noreply.github.com>
2020-07-20 18:51:11 -0400

    listo

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,34 @@
 def linear_search(arr, target):
-    # Your code here
+    for i in arr:
+        if i == target:
+            return i
+        else:
+            return -1
 
 
-    return -1   # not found
+      # not found
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
+    low = 0
+    high = len(arr) - 1
+    while low <= high:
+        mid  = (high + low) // 2
 
-    # Your code here
+        if target == mid:
+            return mid
+
+        if target < arr[mid]:
+            high = mid - 1
+        
+        if target > arr[mid]:
+            low = mid + 1 
+
+    return -1        
 
 
-    return -1  # not found
+
+
+
+    # return -1  # not found
